3eab325404e340a8d2d090dce9936d2cc1837001
Uncle Sam <djiasara@gmail.com>
2020-03-06 10:47:28 -0500

    implementing quotes class

--- db-service/src/main/java/com/revature/stock/dbservice/model/Quotes.java
+++ db-service/src/main/java/com/revature/stock/dbservice/model/Quotes.java
@@ -1,5 +1,48 @@
 package com.revature.stock.dbservice.model;
 
-public class Quotes {
+import java.util.List;
 
+public class Quotes {
+	
+	private String userName;
+	
+	private List<String> quotes;
+	
+	
+
+	public Quotes() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+
+	public Quotes(String userName, List<String> quotes) {
+		super();
+		this.userName = userName;
+		this.quotes = quotes;
+	}
+
+
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public List<String> getQuotes() {
+		return quotes;
+	}
+
+	public void setQuotes(List<String> quotes) {
+		this.quotes = quotes;
+	}
+	
+	
+	
+	
+		
 }
