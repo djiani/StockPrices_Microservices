cf7c7581b46647fe9909dd6df360a0d5d6880b43
Uncle Sam <djiasara@gmail.com>
2020-03-06 15:50:29 -0500

    implementing deleleMapping(/delete/username)

--- db-service/src/main/java/com/revature/stock/dbservice/resource/DBServiceResource.java
+++ db-service/src/main/java/com/revature/stock/dbservice/resource/DBServiceResource.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -42,6 +43,16 @@ public class DBServiceResource {
 		
 		return getQuotesByUserName(quotes.getUserName());
 	}
+	
+	@DeleteMapping("/delete/{username}")
+	public List<String> delete(@PathVariable("username") final String username) {
+		List<Quote> quotes = quotesRespoistory.findByUserName(username);
+	
+		quotesRespoistory.deleteAll(quotes);
+		
+		return getQuotesByUserName(username);
+	}
+	
 	
 	private List<String> getQuotesByUserName(final String username) {
 		return quotesRespoistory.findByUserName(username)
